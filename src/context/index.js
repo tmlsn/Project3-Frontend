d82c12e87bcea0a8c3774d5e15b32f3c0f01bed7
tmlsn <theo.malless@gmail.com>
2022-04-09 13:56:07 +0200

    set up context

--- src/context/index.js
+++ src/context/index.js
@@ -1,17 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { client } from "client";
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 
-export function AuthContextProvider({children}) {
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  const signup = async (firstName, lastName, email, password) => {
+    const response = await client.post("/auth/signup", {
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+    console.log(response);
+  };
+
+  const login = async (email, password) => {
+    const response = await client.post("/auth/login", {
+      email,
+      password,
+    });
+    console.log(response);
+  };
 
   const value = {
-    user: {
-      email: 'a@a.com'
-    }
-  }
+    user,
+    signup,
+    login,
+  };
 
-  return <AuthContext.Provider value={value}>
-    {children}
-  </AuthContext.Provider>
-
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
